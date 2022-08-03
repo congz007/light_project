4c9d113c7aa0c5545d0656a3cd895a9919d1b381
Congon <congz@xtbg.ac.cn>
2022-08-03 14:56:29 +0800

    method and result and qmd

--- scripts/merge-picture.R
+++ scripts/merge-picture.R
@@ -1,8 +1,8 @@
 library(magick)
 library(cowplot)
-ima1 <- ggdraw()+draw_image('figs/sp1-dis.png',width = 1)
-ima2 <- ggdraw()+draw_image('figs/sp2-dis.png',width = 1)
-ima3 <- ggdraw()+draw_image('figs/daylight.jpg',width = 1)
+ima1 <- ggdraw()+draw_image('figs/sp1-dis.png',width = 1, height = 1)
+ima2 <- ggdraw()+draw_image('figs/sp2-dis.png',width = 1,height = 1)
+ima3 <- ggdraw()+draw_image('figs/daylight.jpg',width = 1,height = 1)
 ima3
-ima4 <- ggdraw()+draw_image('figs/night.jpg',width = 1)
+ima4 <- ggdraw()+draw_image('figs/night.jpg',width = 1,height = 1)
 plot_grid(ima1,ima3,ima2,ima4, labels = "AUTO",rel_widths = 1,rel_heights = 1)
