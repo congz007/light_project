4497c9fb9385bf86aa475f686ef31114b7b8101c
Congon <congz@xtbg.ac.cn>
2023-06-28 13:16:56 +0800

    first merge

--- scripts/merge-picture.R
+++ scripts/merge-picture.R
@@ -1,11 +1,11 @@
 library(magick)
 library(cowplot)
-ima1 <- ggdraw()+draw_image('figs/sp1-dis.png',width = 1, height = 1)
-ima2 <- ggdraw()+draw_image('figs/sp2-dis.png',width = 1,height = 1)
-ima3 <- ggdraw()+draw_image('figs/daylight.jpg',width = 1,height = 1)
+library(ggplot2)
+
+ima3 <- ggdraw()+draw_image("figs/daylight.jpg",width = 1,height = 1)
 ima3
 ima4 <- ggdraw()+draw_image('figs/night.jpg',width = 1,height = 1)
-plot_grid(ima1,ima3,ima2,ima4, labels = "AUTO",rel_widths = 1,rel_heights = 1)
 
-
+png("figs/merge.png")
 plot_grid(ima3,ima4,labels = "AUTO",scale = 0.8)
+dev.off()
