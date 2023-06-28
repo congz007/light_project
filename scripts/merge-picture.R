e6c86e7512f055306530138a82c0ea7655816c4c
Congon <congz@xtbg.ac.cn>
2023-06-28 13:49:19 +0800

    small change and cover letter

--- scripts/merge-picture.R
+++ scripts/merge-picture.R
@@ -2,10 +2,18 @@ library(magick)
 library(cowplot)
 library(ggplot2)
 
-ima3 <- ggdraw()+draw_image("figs/daylight.jpg",width = 1,height = 1)
-ima3
-ima4 <- ggdraw()+draw_image('figs/night.jpg',width = 1,height = 1)
+ima3 <- ggdraw()+draw_image("figs/daylight.jpg",width = 1.2,height = 1.2)
+ima4 <- ggdraw()+draw_image('figs/night.jpg',width = 1.2,height = 1.2)
 
-png("figs/merge.png")
-plot_grid(ima3,ima4,labels = "AUTO",scale = 0.8)
+png("figs/merge.png",
+    width = 110,
+    height = 50,
+    units = "mm",
+    bg = "white",
+    res = 300)
+plot_grid(ima3,ima4,labels = "AUTO",scale = 0.7)
 dev.off()
+
+p <- plot_grid(ima3,ima4,labels = "AUTO",scale = 0.8)
+save_plot("merge.png", p, base_asp = 1.1)
+
