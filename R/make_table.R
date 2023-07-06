ce1119a7d2c3cd71a587ffda174039296ae36757
Congon <congz@xtbg.ac.cn>
2023-07-06 11:21:58 +0800

    table rejust

--- R/make_table.R
+++ R/make_table.R
@@ -9,14 +9,14 @@ make_table <- function(data) {
 }
 
 table_summary <- function(summary_stan){
-  parameters <- rep(c("ALAN's effect",
-                   "Daylight's effect",
-                   "interaction"),2)
+  parameters <- rep(c("ALAN",
+                   "Daylight",
+                   "ALAN \u00D7 Daylight"),2)
   quantile_interval <-  paste0("[",round(summary_stan$q2.5,4),", ",  round(summary_stan$q97.5,4),"]")
   mean <- round(summary_stan$mean,4)
 
   tibble(parameters, mean, quantile_interval) |> 
-    kbl(col.names = c("Parameters", "Mean value", "95% CI"),
+    kbl(col.names = c("Variables", "Mean", "95% CI"),
         booktabs = T) |>
     kable_classic(full_width = F) |>
     column_spec(3,bold = c(F,F,F,T,T,F)) |>
